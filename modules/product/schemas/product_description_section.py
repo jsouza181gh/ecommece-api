74e1d84d4c3f8106d42be2e87816e4da58f65be6
Jsouza181gh <emanuelrodrigues010@gmail.com>
2026-06-05 16:26:33 -0300

    feat(product-description): add product description dto schemas

---
+++ modules/product/schemas/product_description_section.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, ConfigDict
+from uuid import UUID
+
+class SaveProductDescriptionSectionSchema(BaseModel):
+    product_id: UUID
+    title: str
+    subtitle: str
+    descrition: str
+
+
+class ProductDescriptionSectionSchema(BaseModel):
+    id: UUID
+    product_id: UUID
+    title: str
+    subtitle: str
+    descrition: str
+    position: int
+
+    model_config = ConfigDict(from_attributes=True)
